198f928f2a12986fa698cc9742f86c18d8b26edc
Aum Singhal <pycoder77@gmail.com>
2021-02-15 16:20:34 +0000

    bam

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from discord.ext import commands, tasks
 from random import choice
 import os
 
+# BotLink = 'https://discord.com/api/oauth2/authorize?client_id=810896982037233664&permissions=2147483639&scope=bot'
+
 
 client = commands.Bot(command_prefix=commands.when_mentioned_or("%"), description='A simple helping bot for Team Shadow!')
 client.remove_command('help')
@@ -38,6 +40,7 @@ async def help(ctx):
   )
   embed.add_field(name='ping', value='Helps you check the latency. type `%ping` to use the command',inline=False)
   embed.add_field(name='invite', value='Gives you the invite link for inviting people to the server and help us grow this server. \nThanks in advance for inviting people 😊')
+  embed.add_field(name='bam', value='You get BAMMED!!')
   embed.set_footer(icon_url=ctx.author.avatar_url, text=f"This message was requested by {ctx.author.name}")
   await ctx.send(embed=embed)
 
@@ -60,6 +63,16 @@ async def invite(ctx):
     )
   embed.set_footer(text='Help Us grow by inviting people.')
   await ctx.send(embed=embed)
+
+
+@client.command()
+async def bam(ctx):
+  embed = discord.Embed(
+    title = f"{ctx.author.name} Got Bammed!!",
+    colour = colour()
+  )
+  embed.set_image(url = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/447790b1-04f1-4ed2-9001-15e6b6f2dfaf/d6yin79-b9e8723a-4866-4cb1-9706-19670676e5f6.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNDQ3NzkwYjEtMDRmMS00ZWQyLTkwMDEtMTVlNmI2ZjJkZmFmXC9kNnlpbjc5LWI5ZTg3MjNhLTQ4NjYtNGNiMS05NzA2LTE5NjcwNjc2ZTVmNi5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.w_vE88Hd7ju-bsXRPm3DMGLl4G0Rzr-fn70i-x3Iq8U')
+  await ctx.send(embed = embed)
 
 
 
